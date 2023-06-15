16db3e5e3d941f90791bf57e6cc2219da5f0046f
aryarasyid <74165903+aryarasyid@users.noreply.github.com>
2023-06-15 14:25:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+}
+function muncultombol2(){fungtom2=1;TombolWA.style="opacity:1;transform:scale(1)";}
+function menuju(){if(fungtom2==1){window.location = "https://api.whatsapp.com/send?phone=&text=" + pesanwhatsapp;}}
